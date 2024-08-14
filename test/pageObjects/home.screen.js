e5cebad50e02da1330e7e1b96c2959e07269ff44
igor andrade <igor.andrade@zigpay.com.br>
2024-08-14 09:39:18 -0300

    continuação do desafio

--- test/pageObjects/home.screen.js
+++ test/pageObjects/home.screen.js
@@ -1,6 +1,6 @@
 export default class HomeScreen{
     get btnSearch() { return $('android=new UiSelector().resourceId("org.wikipedia.alpha:id/search_container")')}
-    get searchResult() { return $(`android=new UiSelector().className("android.view.ViewGroup").instance(1).childSelector(new UiSelector().text("Sport Club Corinthians Paulista"))`)}
+    get searchResult() { return $('android=new UiSelector().className("android.view.ViewGroup").instance(1).childSelector(new UiSelector().text("Sport Club Corinthians Paulista"))')}
     get inpSearch() { return $('android=new UiSelector().resourceId("org.wikipedia.alpha:id/search_src_text")')}
     get btnMore() { return $('android=new UiSelector().resourceId("org.wikipedia.alpha:id/nav_tab_more")')}
 
--- test/specs/validarLoginComCredenciaisInvalidas.js
+++ test/specs/validarLoginComCredenciaisInvalidas.js
@@ -1,20 +1,24 @@
-import HomeScreen from "../pageObjects/home.screen"
-import LoginScreen from "../pageObjects/login.screen"
-import MoreScreen from "../pageObjects/more.screen"
-import OnboardingScreen from "../pageObjects/onboarding.screenn"
+import HomeScreen from "../pageObjects/home.screen.js"
+import LoginScreen from "../pageObjects/login.screen.js"
+import MoreScreen from "../pageObjects/more.screen.js"
+import OnboardingScreen from "../pageObjects/onboarding.screen.js"
 
 describe('Wikipedia', () =>{
     it('Validar Login com credenciais invalidas', async () => {
-        const onboarding = new OnboardingScreen
-        const homescreen = new HomeScreen
-        const more = new MoreScreen
-        const login = new LoginScreen
+        const onboarding = new OnboardingScreen()
+        const homescreen = new HomeScreen()
+        const more = new MoreScreen()
+        const login = new LoginScreen()
         await onboarding.clickBtnSkip()
         await homescreen.clickBtnMore()
         await more.clickBtnLogin()
-        await login.ClickBtnEntrar()
-        await login.username(teste)
-        await login.password(teste)
-        // expect(await homescreen.getSearchResult()).toBe('Sport Club Corinthians Paulista')
+        await login.clickBtnEntrarLogin()
+        await login.username()
+        await login.setUsername("teste")
+        await login.password()
+        await login.setPassword("teste")
+        await login.clickBtnEntrar()
+        await login.getResponseTxt()
+        exxpect(await login.getResponseTxt()).toBe('O nome de utilizador ou a palavra-passe inseridos estão incorretos. Tente novamente, por favor.')
     })
 })
